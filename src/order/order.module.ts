f5a60a5ea80da00c4f56e2199c750470407e2182
Tom1101 <tuananhnguyen1101@gmail.com>
2019-02-03 17:00:10 +0100

    update api

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -3,10 +3,12 @@ import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderTab } from './order.entity';
+import { CustomerService } from '../customer/customer.service';
+import { CustomerController } from '../customer/customer.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([OrderTab])],
-  providers: [OrderService],
-  controllers: [OrderController],
+  providers: [OrderService, CustomerService],
+  controllers: [OrderController, CustomerController],
 })
 export class OrderModule {}
